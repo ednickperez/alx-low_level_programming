139a458003cc869ead5f4dfcd40bf84f987aa3db
ednickperez <technicaltutor33@gmail.com>
2022-09-14 07:23:14 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
 #include "alx.h"
-
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
+ *  * main - Prints Holberton
+ *    * Return: Always 0 (Success)
  **/
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('A');
+	_putchar('L');
+	_putchar('X');
+	_putchar('\n');
+	return (0);
 }
